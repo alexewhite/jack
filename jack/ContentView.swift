b1c694dc5fe7fe97779288ce60bbd74ca21d1d5a
Alex White <alex.eliz.white@gmail.com>
2025-08-11 17:22:20 -0400

    added texts

--- jack/ContentView.swift
+++ jack/ContentView.swift
@@ -10,14 +10,39 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack (spacing: 0.0){
-            Color.blue.opacity(0.25)
-                .ignoresSafeArea(edges: .all)
-                .frame(maxHeight: .infinity)
-                .frame(height: UIScreen.main.bounds.height * (6/10))
-            Color.blue.opacity(0.9)
-                .ignoresSafeArea(edges: .all)
-        }
-        
+            ZStack{
+                Color(red: 0/255, green: 51/255, blue: 102/255)
+                    .ignoresSafeArea(edges: .all)
+                    .frame(maxHeight: .infinity)
+                    .frame(height: UIScreen.main.bounds.height * (28/50))
+                Text(" Welcome to Learn & Leaf")
+                    .font(.custom("Arial", size: 42))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(20.0)
+            } // end of ZStack
+            ZStack{
+                Color.blue.opacity(0.2)
+                    .ignoresSafeArea(edges: .all)
+                VStack (spacing: 10){
+                    HStack{
+                        Text("Our Mission")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.leading)
+                        Text("                                        ")
+                    } // end of HStack
+                    
+                    Text("With Leaf & Learn, learning doesn’t always have to feel like school, you don’t need a teacher or a classroom to learn, just our app. We want to provide easier access to education, and make it easier to gain access to extra-help resources. Our inspiration came from our passion for education and wanting to teach others and inspire people to chase their curiosity.")
+                    Spacer()
+                } // end of VStack
+                .padding(.top, 30)
+                .padding(.horizontal, 30)
+            } // end of ZStack
+            
+        } // end of VStack
+            
     }
 }
 
