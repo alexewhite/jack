8e5353ec6ea30e275ddc3b462c5801ea3fd8fa7e
Alex White <alex.eliz.white@gmail.com>
2025-08-11 18:15:06 -0400

    added navigation view and navigaiton link

---
+++ jack/MenuBar.swift
@@ -0,0 +1,18 @@
+//
+//  MenuBar.swift
+//  jack
+//
+//  Created by Alex White on 8/11/25.
+//
+
+import SwiftUI
+
+struct MenuBar: View {
+    var body: some View {
+        Text("Menu")
+    }
+}
+
+#Preview {
+    MenuBar()
+}
