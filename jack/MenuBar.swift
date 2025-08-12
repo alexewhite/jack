48b2bbba7725512f8198393e7432cc120a23c023
Alex White <alex.eliz.white@gmail.com>
2025-08-11 21:52:09 -0400

    added a navigation stack and navigation links to the menu page

--- jack/MenuBar.swift
+++ jack/MenuBar.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct MenuBar: View {
     var body: some View {
-        Text("Menu")
+        NavigationStack {
+            ZStack{
+                Color(red: 0.0, green: 0.39, blue: 0.0)
+                    .ignoresSafeArea()
+                VStack (alignment: .leading, spacing: 10){
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Text("Creativity")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                    }
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Text("Education")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                    }
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Text("Log In")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                    }
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Text("Goals")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                    }
+                } // end of VStack
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .padding(.leading, 20)
+                .padding(.horizontal, 20)
+            } // end of ZStack
+        } // end of navigation stack
     }
 }
 
